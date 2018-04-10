83d2f021c39e23d347d87056f07321a56c88b99a
Roy <619821473@qq.com>
2018-04-10 20:25:25 +0800

    init commit

--- src/q1.java
+++ src/q1.java
@@ -14,16 +14,19 @@ import java.util.Arrays;
  */
 public class q1 {
     public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
                 if (i != j){
                     if (target == nums[i] + nums[j]){
-                        return new int[]{i, j};
+                        result[0] = i;
+                        result[1] = j;
+                        return result;
                     }
                 }
             }
         }
-        return new int[]{0, 0};
+        return result;
     }
 
     public static void main(String[] args) {
