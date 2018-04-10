c4903f510b937825c7051ae5af8d408fb8a56631
Roy <619821473@qq.com>
2018-04-10 17:16:10 +0800

    init commit

--- src/q1.java
+++ src/q1.java
@@ -1,5 +1,34 @@
+import java.util.Arrays;
+
 /**
- * Created by Roy on 2018/4/10.
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+ Example:
+
+ Given nums = [2, 7, 11, 15], target = 9,
+
+ Because nums[0] + nums[1] = 2 + 7 = 9,
+ return [0, 1].
  */
 public class q1 {
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j){
+                    if (target == nums[i] + nums[j]){
+                        return new int[]{i, j};
+                    }
+                }
+            }
+        }
+        return new int[]{0, 0};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
 }
