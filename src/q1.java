4d861452d7221fbc4eeca388b36ed96e612fe9dd
Roy <619821473@qq.com>
2018-04-12 15:24:22 +0800

    加入第五题

--- src/q1.java
+++ src/q1.java
@@ -1,16 +1,16 @@
 import java.util.Arrays;
 
 /**
- * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * 给定一个整数数列，找出其中和为特定值的那两个数。
 
- You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ 你可以假设每个输入都只会有一种答案，同样的元素不能被重用。
 
- Example:
+ 示例:
 
- Given nums = [2, 7, 11, 15], target = 9,
+ 给定 nums = [2, 7, 11, 15], target = 9
 
- Because nums[0] + nums[1] = 2 + 7 = 9,
- return [0, 1].
+ 因为 nums[0] + nums[1] = 2 + 7 = 9
+ 所以返回 [0, 1]
  */
 public class q1 {
     public static int[] twoSum(int[] nums, int target) {
